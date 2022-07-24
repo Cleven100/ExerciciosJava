caa47aa0cbb944d8b39d78506f36f8b1983c70d9
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-24 15:59:18 -0300

    24/07

--- bytebank/src/App.java
+++ bytebank/src/App.java
@@ -1,5 +1,19 @@
+import entitites.Conta;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Conta conta = new Conta();
+
+        conta.setSaldo(200.00);
+
+        System.out.println(conta.getSaldo());
+
+        conta.adicionarSaldo(100);
+
+        System.out.println(conta.getSaldo());
+
+        conta.removerSaldo(50);
+
+        System.out.println(conta.getSaldo());
     }
 }
