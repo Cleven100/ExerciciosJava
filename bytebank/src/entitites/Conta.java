ceb646d4b7bbde03e82b1639d9afe0c09ca9cb45
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-24 16:02:45 -0300

    bytebank

--- bytebank/src/entitites/Conta.java
+++ bytebank/src/entitites/Conta.java
@@ -1,47 +1,31 @@
 package entitites;
 
 public class Conta {
-    private double saldo;
-    private int agencia;
-    private int numero;
-    private String titular;
 
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
 
-
-    public double getSaldo() {
-        return saldo;
-    }
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-    public int getAgencia() {
-        return agencia;
-    }
-    public void setAgencia(int agencia) {
-        this.agencia = agencia;
-    }
-    public int getNumero() {
-        return numero;
-    }
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-    public String getTitular() {
-        return titular;
-    }
-    public void setTitular(String titular) {
-        this.titular = titular;
+    public void deposita(double valor) {
+        this.saldo = this.saldo + valor;
     }
 
-    public void adicionarSaldo(double saldo) {
-        this.saldo = this.saldo + saldo;
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
     }
-  
-    public void removerSaldo(double saldo) {
-        this.saldo = this.saldo - saldo;
+
+    public boolean transfere(double valor, Conta destino) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
     }
-    
-
-
-
 }
