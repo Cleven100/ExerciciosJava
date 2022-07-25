cb6b2dc7f830ffba77c59377ca4f3a2d74ed011f
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-25 19:59:12 -0300

    25/07

--- bytebank-herdado/src/App.java
+++ bytebank-herdado/src/App.java
@@ -1,8 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
        
-         Funcionario func = new Funcionario();
+      /*    Funcionario teste = new Funcionario();
 
+         teste.setNome("Teste");
+         teste.setCpf("000000000");
+         teste.setSalario(2500);
+
+         System.out.println(teste.getNome());
+         System.out.println(teste.getBonificacao());  */
+ 
+
+         FuncionarioTeste testeTwo = new FuncionarioTeste();
+          testeTwo.setSalario(3000);
+       
+         System.out.println(testeTwo.getTipo());
+         System.out.println(testeTwo.getBonificacao());
 
 
     }
