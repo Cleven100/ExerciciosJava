b846a656fa07f8c7ecaf9b0f98c8e787e2340f3e
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-30 08:37:55 -0300

    Exercicio_3

--- Lista de exercicio/Exercicio_3/src/App.java
+++ Lista de exercicio/Exercicio_3/src/App.java
@@ -1,5 +1,29 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double noteOne, noteTwo, noteThree, average;
+        int weightOne, weightTwo, weightThree;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the first note: ");
+        noteOne = sc.nextDouble();
+        System.out.println("Write the first weight: ");
+        weightOne = sc.nextInt();
+        System.out.println("Write the second note: ");
+        noteTwo = sc.nextDouble();
+        System.out.println("Write the second weight: ");
+        weightTwo = sc.nextInt();
+        System.out.println("Write the third note: ");
+        noteThree = sc.nextDouble();
+        System.out.println("Write the third weight: ");
+        weightThree = sc.nextInt();
+
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        average = ((noteOne * weightOne) + (noteTwo * weightTwo)+ (noteThree * weightThree)) / (weightOne + weightTwo + weightThree);
+
+        System.out.println("The result: " + df.format(average));
     }
 }
