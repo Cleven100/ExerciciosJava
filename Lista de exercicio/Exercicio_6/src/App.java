3032dfaebf234ca904303aaa8b3c183ab29fc2c1
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-01 08:13:13 -0300

    Exercicio_6

--- Lista de exercicio/Exercicio_6/src/App.java
+++ Lista de exercicio/Exercicio_6/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salary;
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write the salary of employee: ");
+        salary = sc.nextDouble();
+       
+        
+        System.out.println("The salary of employee is: " + ((salary * 1.05) - (salary * 0.07)));
     }
 }
